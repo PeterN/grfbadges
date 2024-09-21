46e8daffa576b52f40da451196a754b931542de1
Peter Nelson <peter@fuzzle.org>
2024-09-21 08:36:34 +0100

    Rewrite with Badge and Badges as SpriteGenerator
    This allows re-use in different NewGRFs.

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # pip install git+https://github.com/citymania-org/grf-py.git@ae4aeab54638cc206d3c969caae0e473a8636651#egg=grf
 
 import io
@@ -9,12 +8,9 @@ from PIL import Image
 import lib
 import grf
 
-THIS_FILE = grf.PythonFile(__file__)
-
-ZOOM = grf.ZOOM_2X
-BPP = grf.BPP_32
-
 BADGE_PATH = Path("badges")
+BADGE_ZOOM = grf.ZOOM_2X
+BADGE_BPP = grf.BPP_32
 
 # Base sizes at 1x zoom.
 BADGE_HEIGHT = int(12)
@@ -23,50 +19,68 @@ FLAG_WIDTH = int(FLAG_HEIGHT * 3 / 2) # Force flags to have a 3:2 aspect ratio t
 
 BADGE = lib.register_badge_feature()
 
-g = grf.NewGRF(
-    format_version=8,
-    grfid=b"PNS\xFF",
-    name="Default Badge Set",
-    description="A set of badges for common country flags, power types and operator logos",
-    id_map_file="id_map.json",
-)
+class BadgeOverlays:
+    def __init__(self, name):
+        self.name = name
+        self.overlays = {}
 
-s = grf.StringManager()
-s.import_lang_dir(str(BADGE_PATH / "lang"))
+    def get_overlay(self, zoom):
+        if zoom not in self.overlays:
+            if zoom == grf.ZOOM_NORMAL:
+                self.overlays[zoom] = grf.Image.open(BADGE_PATH / f"{self.name}-{FLAG_WIDTH}x{FLAG_HEIGHT}.png")
+            else:
+                scale = lib.get_scale_for_zoom(zoom)
+                self.overlays[zoom] = self.get_overlay(grf.ZOOM_NORMAL).resize(size=(FLAG_WIDTH * scale, FLAG_HEIGHT * scale), resample=Image.Resampling.NEAREST)
 
-class Badge:
+        return self.overlays[zoom]
+
+# Overlay for flags to provide shading and texture. This also subdues the flag colours slightly which makes the flags
+# less accurate, but actually makes them fit in better.
+flag_overlays = BadgeOverlays("flag-overlay")
+
+class Badge(grf.SpriteGenerator):
     def __init__(self, id, label, image, string, flags=None):
         if len(grf.to_bytes(bytes(label, "utf-8").decode("unicode_escape"))) != 4:
             raise ValueError("label must be 4 bytes")
+        if string is not None and not isinstance(string, grf.StringRef):
+            raise ValueError("string must be None or a StringRef")
 
-        self.id = g.resolve_id(BADGE, label) if id is None else id
+        self.id = id
         self.label = label
         self.image = image
         self.string = string
         self.flags = flags
 
-class Badges:
-    def __init__(self):
+    def get_sprites(self, g):
+        # In the interests of code reuse, and laziness, this produces a 'batch' of just one badge.
+        badges = Badges()
+        badges.badges.append(self)
+        return badges.get_sprites(g)
+
+class Badges(grf.SpriteGenerator):
+    def __init__(self, s = None):
         self.badges = []
         self.next_id = 0
+        if s is None:
+            self.s = grf.StringManager()
+            self.s.import_lang_dir(str(BADGE_PATH / "lang"))
+        else:
+            self.s = s
 
     def add(self, label, image, string, flags=None):
-        self.badges.append(Badge(self.next_id, label, image, None if string is None else s[string], flags))
+        self.badges.append(Badge(self.next_id, label, image, None if string is None else self.s[string], flags))
         self.next_id += 1
 
-badges = Badges()
-
-from badges import define_badges
-
-define_badges(badges)
-
-# Overlay for flags to provide shading and texture. This also subdues the flag colours slightly which makes the flags
-# less accurate, but actually makes them fit in better.
-flag_overlay = {}
-flag_overlay[grf.ZOOM_NORMAL] = grf.Image.open(BADGE_PATH / f"flag-overlay-{FLAG_WIDTH}x{FLAG_HEIGHT}.png")
-for zoom in [grf.ZOOM_2X, grf.ZOOM_4X]:
-    scale = lib.get_scale_for_zoom(zoom)
-    flag_overlay[zoom] = flag_overlay[grf.ZOOM_NORMAL].resize(size=(FLAG_WIDTH * scale, FLAG_HEIGHT * scale), resample=Image.Resampling.NEAREST)
+    def get_sprites(self, g):
+        return [
+            # Define properties
+            lib.PropertyBatcher(BADGE, "label", ((b.id, grf.to_bytes(bytes(b.label, "utf-8").decode("unicode_escape"))) for b in self.badges if b.label is not None)),
+            lib.PropertyBatcher(BADGE, "flags", ((b.id, b.flags) for b in self.badges if b.flags is not None)),
+            # Define names
+            lib.StringBatcher(BADGE, ((b.id, b.string) for b in self.badges)),
+            # Define sprites
+            BadgeSpriteBatch([b for b in self.badges if b.image is not None]),
+        ]
 
 class BadgeSprite(grf.Sprite):
     def __init__(self, badge, zoom):
@@ -105,7 +119,7 @@ class SvgFlagSprite(BadgeSprite):
         flag = svg2png(url=self.file.path, output_height=self.h)
         f = io.BytesIO(flag)
         im = grf.Image.open(f).convert(mode="RGBA").resize((self.w, self.h))
-        im = Image.blend(im, flag_overlay[self.zoom], 0.25)
+        im = Image.blend(im, flag_overlays.get_overlay(self.zoom), 0.25)
         return im, grf.BPP_32
 
 class SvgBadgeSprite(BadgeSprite):
@@ -164,13 +178,13 @@ class BadgeSprites(grf.SpriteGenerator):
 
     def make_badge_bpps(self, sprite):
         sprites = []
-        if BPP == grf.BPP_32:
+        if BADGE_BPP == grf.BPP_32:
             sprites.append(sprite)
         sprites.append(grf.QuantizeSprite(sprite))
         return sprites
 
-    def make_badge_from_svg(self, zoom, isFlag):
-        sprite = SvgFlagSprite(self.badge, zoom) if isFlag else SvgBadgeSprite(self.badge, zoom)
+    def make_badge_from_svg(self, zoom, is_flag):
+        sprite = SvgFlagSprite(self.badge, zoom) if is_flag else SvgBadgeSprite(self.badge, zoom)
         return self.make_badge_bpps(sprite)
 
     def make_badge_from_image(self, im, zoom):
@@ -181,15 +195,15 @@ class BadgeSprites(grf.SpriteGenerator):
         sprites = []
         if self.badge.image.endswith(".svg"):
             # Badges in the flag class have the flag overlay applied
-            isFlag = self.badge.label.startswith("f")
-            sprites.extend(self.make_badge_from_svg(grf.ZOOM_NORMAL, isFlag))
-            if ZOOM == grf.ZOOM_2X or ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_svg(grf.ZOOM_2X, isFlag))
-            if ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_svg(grf.ZOOM_4X, isFlag))
+            is_flag = self.badge.label[0] == "f"
+            sprites.extend(self.make_badge_from_svg(grf.ZOOM_NORMAL, is_flag))
+            if BADGE_ZOOM == grf.ZOOM_2X or BADGE_ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_svg(grf.ZOOM_2X, is_flag))
+            if BADGE_ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_svg(grf.ZOOM_4X, is_flag))
         else:
             im = ReusableImage(self.filename)
             sprites.extend(self.make_badge_from_image(im, grf.ZOOM_NORMAL))
-            if ZOOM == grf.ZOOM_2X or ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_image(im, grf.ZOOM_2X))
-            if ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_image(im, grf.ZOOM_4X))
+            if BADGE_ZOOM == grf.ZOOM_2X or BADGE_ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_image(im, grf.ZOOM_2X))
+            if BADGE_ZOOM == grf.ZOOM_4X: sprites.extend(self.make_badge_from_image(im, grf.ZOOM_4X))
 
         return [
             grf.AlternativeSprites(*sprites)
@@ -204,7 +218,8 @@ class BadgeSpriteBatch(grf.SpriteGenerator):
         res.append(grf.Action1(feature=BADGE, set_count=len(self.badges), sprite_count=1))
 
         for b in self.badges:
-            res.append(BadgeSprites(b))
+            b.sprites = BadgeSprites(b)
+            res.append(b.sprites)
 
         act1id = 0
         for b in self.badges:
@@ -212,15 +227,3 @@ class BadgeSpriteBatch(grf.SpriteGenerator):
             act1id += 1
 
         return res
-
-# Define properties
-g.add(lib.PropertyBatcher(BADGE, "label", ((b.id, grf.to_bytes(bytes(b.label, "utf-8").decode("unicode_escape"))) for b in badges.badges if b.label is not None)))
-g.add(lib.PropertyBatcher(BADGE, "flags", ((b.id, b.flags) for b in badges.badges if b.flags is not None)))
-
-# Define names
-g.add(lib.StringBatcher(BADGE, ((b.id, b.string) for b in badges.badges)))
-
-# Define sprites
-g.add(BadgeSpriteBatch([b for b in badges.badges if b.image is not None]))
-
-grf.main(g, "badges.grf")
