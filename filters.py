b92e9dd2c4ecdd27cf6aad33cdb47070bfac2bff
Peter Nelson <peter@fuzzle.org>
2024-09-26 18:38:51 +0100

    Add SpriteMasker classes

--- filters.py
+++ filters.py
@@ -8,6 +8,45 @@ import grf
 rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
 hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
 
+class SpriteMasker:
+    def make_mask(self, rgb):
+        raise NotImplementedError
+
+    def get_fingerprint(self):
+        raise NotImplementedError
+
+class HueMasker(SpriteMasker):
+    def __init__(self, hue):
+        self.hue = hue / 360.
+
+    def make_mask(self, rgb):
+        r, g, b, a = np.rollaxis(rgb, axis=-1)
+        h, s, v = rgb_to_hsv(r, g, b)
+        m = (v > 31) & (s >= .1) & (h == self.hue)
+        return m
+
+    def get_fingerprint(self):
+        return {
+            'class': self.__class__.__name__,
+            'hue': self.hue,
+        }
+
+class SaturationMasker(SpriteMasker):
+    def __init__(self, sat):
+        self.sat = sat
+
+    def make_mask(self, rgb):
+        r, g, b, a = np.rollaxis(rgb, axis=-1)
+        h, s, v = rgb_to_hsv(r, g, b)
+        m = s >= self.sat
+        return m
+
+    def get_fingerprint(self):
+        return {
+            'class': self.__class__.__name__,
+            'sat': self.sat,
+        }
+
 class SpriteFilter:
     def apply_filter(self, sprite):
         raise NotImplementedError
